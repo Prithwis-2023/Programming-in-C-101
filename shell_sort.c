645d5811fc5dfdfde6287ebb2fae377e8f236265
Prithwis Das <prithwisdas2017@gmail.com>
2024-10-18 18:11:39 -0700

    upload shell sort

---
+++ shell_sort.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void ShellSort(int arr[], int n);
+
+int main (int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        return 1;
+    }    
+
+    int size = atoi(argv[1]);
+
+    int *nums = malloc(size * sizeof(int));
+
+    if (nums == NULL)
+    {
+        return 1;
+    }
+
+    for (int i = 0; i < size; i++) {scanf("%i", &nums[i]);};
+
+    ShellSort(nums, size);
+
+    for (int i = 0; i < size; i++) {printf("%i ", nums[i]);};
+
+}
+
+void ShellSort(int arr[], int n)
+{
+    for (int gap = n / 2; gap > 0; gap /= 2)
+    {
+        for (int i = gap; i < n; i++)
+        {
+            int temp = arr[i];
+
+            int j;
+
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+            {
+                arr[j] = arr[j - gap];
+            }
+
+            arr[j] = temp;
+        }
+    }
+}

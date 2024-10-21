d7c8518046b8cf495969ac3c03d2e2919524c6b9
Prithwis Das <prithwisdas2017@gmail.com>
2024-10-21 10:26:55 -0700

    update

--- heap_sort.c
+++ heap_sort.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void heapify(int arr[], int size, int i);
+
 int main (int argc, char *argv[])
 {
     if (argc != 2)
@@ -12,12 +14,34 @@ int main (int argc, char *argv[])
 
     int *nums = malloc(size * sizeof(int));
 
+    if (nums == NULL)
+    {
+        return 1;
+    }
+
     for (int i = 0; i < size; i++) {scanf("%i", &nums[i]);};
 
     for (int i = size / 2 - 1; i >= 0; i--)
     {
         heapify(nums, size, i);
     }
+
+    for (int i = size - 1; i >= 0; i--)
+    {
+        int temp = nums[0];
+        
+        nums[0] = nums[i];
+        
+        nums[i] = temp;
+
+        heapify(nums, i, 0);
+    }
+    
+    for (int i = 0; i < size; i++) {printf("%i ", nums[i]);};
+    
+    free(nums);
+
+    return 0;
 }
 
 void heapify(int arr[], int size, int i)
